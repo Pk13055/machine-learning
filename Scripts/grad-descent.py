b7d58a8d1a3b2b54720250953da06da8fcd02b88
pk13055 <pkrockstar7@gmail.com>
2017-05-22 01:24:34 +0530

    TESTED AND WORKING

--- Scripts/grad-descent.py
+++ Scripts/grad-descent.py
@@ -68,8 +68,17 @@ def h(thetas, xi_s):
 	return sum([i * j for i, j in zip(thetas, xi_s)])
 
 # this is the core functionality that calculates the various theta values
-def gradDesc(data_set, learning_rate, n, m, tolerance = 0.000001):
+def gradDesc(data_set, learning_rate, n, m, timeout = 30, tolerance = 0.000001):
+	# imports for the function
+	from math import isnan
+	import datetime
+
+	# return theta for the current run
 	theta = [ 0 for _ in range(n)]
+	
+	# master break for long processes
+	break_button = datetime.datetime.now()
+
 	while True:
 		current_theta = [ 0 for _ in range(n) ]
 		for _ in range(n):
@@ -77,15 +86,28 @@ def gradDesc(data_set, learning_rate, n, m, tolerance = 0.000001):
 			new_theta = theta[_] - (learning_rate / m) * partial_sum
 			current_theta[_] = new_theta
 		print(current_theta)
+		
+		# check for inf 
+		if True in list(map(lambda x: isnan(x), theta)):
+			print("THETA(S) HAS/HAVE DIVERGED", "RESULTS WILL BE INCORRECT", sep = "\n")
+			break
+
+		# check for convergence
 		if close_enough(current_theta, theta, tolerance):
 			theta = current_theta
 			break
+
+		# check for time exceed
+		if (datetime.datetime.now() - break_button).seconds > timeout:
+			print("TIME EXCEEDED, FLUSHING NOW", "RESULTS WILL BE INCORRECT", sep = "\n")
+			break
+		
 		theta = current_theta
 	return theta
 
 
 # this is the  functionality that parses the various params required for gradient descent 
-def calc_params(learning_rate, data_set):
+def calc_params(learning_rate, data_set, timeout = 30):
 	normal = len(data_set['statistics']) != 0 # bool to check if data is normalized or not
 	stats = data_set['statistics']
 	learning_set = data_set['data_set'] # dataset that we will work with 
@@ -99,22 +121,42 @@ def calc_params(learning_rate, data_set):
 		'n' : n,
 		'm' : m,
 	}
-	return_obj['parameters'] = gradDesc(learning_set, learning_rate, n, m)
+	return_obj['parameters'] = gradDesc(learning_set, learning_rate, n, m, timeout)
 	if normal:
 		return_obj['statistics'] = stats
 	return return_obj
 
+# to normalize the input data if it isn't
+def make_normal(xi_s, stats):
+	if len(xi_s) == len(stats) - 1:
+		return [ (i - j[0]) / j[-1] for i, j in zip(xi_s, stats)]
+	else:
+		return None
+
+
 # once the thetas have been made you can query them as follows
 def query_y(theta, n, stats = []):
-	print(theta, stats)
-	# print("Enter the xi", end = " : ")
-	# xi_s = list(map(float, list(filter( lambda x: x != '', input().strip(' ').split(' ')))))[: n]
-	xi_s = [1234]
+	
+	stats = [tuple([stats[i], stats[i + 1]]) for i in range(0, len(stats), 2)]
+	
+	# this is for taking input from the user
+	print("Enter the xi", end = " : ")
+	xi_s = list(map(float, list(filter( lambda x: x != '', input().strip(' ').split(' ')))))[: n]
+
+	# use next three lines for checking the accuracy of the program
+	# xi_s = [1234]
+	# ./grad-descent.py 0.0099999 dataset2.txt dataset2_normal.txt => normalized
+	# ./grad-descent.py 0.0000000769 dataset2.txt => unormalized 
+
+	# big check here will be to check if the data is normalized and 
+	# the query data needs to be done the same or not
 	xi_s.insert(0,1)
-	value = h(theta, xi_s)
-	print(value, end = ' : ')
 	if len(stats):
-		value = (value * stats[-1]) + stats[-2]
+		xi_s = make_normal(xi_s, stats)
+	value = h(theta, xi_s)
+	if len(stats):
+		print("<USING NORMALIZED DATA>", end = " ")
+		value = (value * stats[-1][-1]) + stats[-1][0]
 	print(value)
 	return value
 
@@ -125,6 +167,10 @@ def main():
 		data_set = sys.argv[2]
 		try:
 			normalized_data = sys.argv[3]
+			try:
+				timeout = float(sys.argv[4])
+			except:
+				timeout = 30
 		except:
 			normalized_data = "normal_data.txt"
 	except:
@@ -134,9 +180,16 @@ def main():
 			normalized_data = ""
 	# grad_obj = gradientDescent(float(learning_rate), data_set, normalized_data)
 	data_set = process_data(data_set, normalized_data)
-	params = calc_params(learning_rate, data_set)
-	query_y(params['parameters'], params['n'], params['statistics'])
-	print(params)
+	params = calc_params(learning_rate, data_set, timeout)
+	answer = True
+	while answer:
+		query_y(params['parameters'], params['n'], params['statistics'])
+		print("Calculate another (Y/n) : ", end = "")
+		ans = str(input())
+		answer = False
+		if ans == "" or ans[0] == "" or ans[0] == "y":
+			answer = True
+	# print(params)
 
 
 
