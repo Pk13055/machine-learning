803f9a06686816eddfb767834cbbb912325b50d5
pk13055 <pkrockstar7@gmail.com>
2017-07-07 02:45:21 +0400

    K-Means v2 complete

--- 1Python/1Supervised/0K-Means/helper.py
+++ 1Python/1Supervised/0K-Means/helper.py
@@ -15,3 +15,6 @@ def is_close(new_centroid, old_centroid):
 	old_centroid = np.matrix(old_centroid)
 	return np.all(np.divide(abs(new_centroid - old_centroid),\
 		 abs(new_centroid)) < config.cent_tolerance)
+
+def is_close_cost(new_J, old_J):
+	return abs(new_J - old_J) / new_J < config.J_tolerance
